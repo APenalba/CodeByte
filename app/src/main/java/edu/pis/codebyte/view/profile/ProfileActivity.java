bca20fc1c9ce7d4cfef594080c0c26935f684453
Alejandro Penalba <apenalbatarrida@gmail.com>
2023-04-20 09:23:19 +0200

    - ProfileViewModel

--- app/src/main/java/edu/pis/codebyte/view/profile/ProfileActivity.java
+++ app/src/main/java/edu/pis/codebyte/view/profile/ProfileActivity.java
@@ -1,8 +1,9 @@
 package edu.pis.codebyte.view.profile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -12,26 +13,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import edu.pis.codebyte.R;
 import edu.pis.codebyte.viewmodel.profile.ProfileViewModel;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
-    private TextView nombreUsuario;
-    private TextView email;
-    private Button cambiarNombre_button;
-    private Button cambiarCorreo_button;
-    private Button cambiarContraseña_button;
+    private TextView username_textView;
+    private TextView email_textView;
     private Button enviaProblema_button;
     private EditText problema;
     private Spinner idioma;
@@ -44,23 +33,18 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mAuth = FirebaseAuth.getInstance();
-        currentUser = mAuth.getCurrentUser();
+        profileVM = new ViewModelProvider(this).get(ProfileViewModel.class);
         activity_setup();
+
 
     }
 
     private void activity_setup() {
 
-        nombreUsuario = (TextView) findViewById(R.id.username_textView);
-        //TODO puede ser nulo el username
-        nombreUsuario.setText(currentUser.getDisplayName());
-
-        email = (TextView) findViewById(R.id.correo_textView);
-        email.setText(currentUser.getEmail());
-
+        username_textView_setup();
+        email_textView_setup();
         cambiarNombre_button_setup();
-        cambiarContraseña_button_setup();
+        cambiarPassword_button_setup();
         cambiarCorreo_button_setup();
 
         enviaProblema_button = (Button) findViewById(R.id.btn_enviaProblema);
@@ -73,73 +57,79 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
+    private void email_textView_setup() {
+        email_textView = (TextView) findViewById(R.id.correo_textView);
+        final Observer<String> observerEmail = new Observer<String>() {
+            @Override
+            public void onChanged(String email) {
+                email_textView.setText(email);
+            }
+        };
+        profileVM.getEmail().observe(this, observerEmail);
+    }
+
+    private void username_textView_setup() {
+        username_textView = (TextView) findViewById(R.id.username_textView);
+        final Observer<String> observerUsername = new Observer<String>() {
+            @Override
+            public void onChanged(String username) {
+                username_textView.setText(username);
+            }
+        };
+        profileVM.getUsername().observe(this, observerUsername);
+
+    }
+
     private void cambiarNombre_button_setup() {
-        cambiarNombre_button = (Button) findViewById(R.id.btn_nombre);
+        Button cambiarNombre_button = (Button) findViewById(R.id.btn_nombre);
         cambiarNombre_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO cambiar nombre
-                mostrarDialogoCambiarNombreUsuario();
+                mostrarDialogo("Cambiar nombre de usuario");
             }
         });
     }
 
-    private void cambiarContraseña_button_setup() {
-        cambiarContraseña_button = (Button) findViewById(R.id.btn_contrasena);
+    private void cambiarPassword_button_setup() {
+        Button cambiarContraseña_button = (Button) findViewById(R.id.btn_contrasena);
         cambiarContraseña_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO cambiar contraseña
+
             }
         });
     }
 
     private void cambiarCorreo_button_setup() {
-        cambiarCorreo_button = (Button) findViewById(R.id.btn_correo);
+        Button cambiarCorreo_button = (Button) findViewById(R.id.btn_correo);
         cambiarCorreo_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO cambiar correo
+
             }
         });
     }
-    private void mostrarDialogoCambiarNombreUsuario() {
+
+    private void mostrarDialogo(String title) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.nuevo_nombre_usuario_dialog, null);
+        View view = getLayoutInflater().inflate(R.layout.input_edittext_dialog, null);
         builder.setView(view);
-        builder.setTitle(R.string.cambiar_nombre_usuario_titulo);
+        builder.setTitle(title);
         builder.setPositiveButton(R.string.guardar, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                EditText etNuevoNombreUsuario = view.findViewById(R.id.et_nuevo_nombre_usuario);
-                String nuevoNombreUsuario = etNuevoNombreUsuario.getText().toString().trim();
-                cambiarNombreUsuario(nuevoNombreUsuario);
-
+                EditText input_editText = view.findViewById(R.id.new_editText);
+                String new_input = input_editText.getText().toString().trim();
+                if(!new_input.isEmpty()) {
+                    System.out.println("AQUI");
+                    profileVM.cambiarNombreUsuario(new_input,ProfileActivity.this);
+                }
             }
         });
         builder.setNegativeButton(R.string.cancelar, null);
         builder.show();
     }
 
-    private void cambiarNombreUsuario(String nuevoNombreUsuario) {
-
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(nuevoNombreUsuario)
-                .build();
-
-        currentUser.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            // El nombre de usuario se ha actualizado correctamente
-                            Toast.makeText(ProfileActivity.this, "Nombre de usuario actualizado", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // Se produjo un error al actualizar el nombre de usuario
-                            Toast.makeText(ProfileActivity.this, "Error al actualizar el nombre de usuario", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-    }
 }
--- app/src/main/java/edu/pis/codebyte/viewmodel/profile/ProfileViewModel.java
+++ app/src/main/java/edu/pis/codebyte/viewmodel/profile/ProfileViewModel.java
@@ -1,6 +1,60 @@
 package edu.pis.codebyte.viewmodel.profile;
 
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import edu.pis.codebyte.view.profile.ProfileActivity;
+
 public class ProfileViewModel extends ViewModel {
+
+    private FirebaseUser currentUser;
+
+    private MutableLiveData<String> username;
+    private MutableLiveData<String> email;
+
+    public ProfileViewModel() {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        username = new MutableLiveData<>(currentUser.getDisplayName());
+        email = new MutableLiveData<>(currentUser.getEmail());
+    }
+
+    public void cambiarNombreUsuario(String new_username, Context context) {
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(new_username)
+                .build();
+        currentUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // El nombre de usuario se ha actualizado correctamente
+                            Toast.makeText(context, "Nombre de usuario actualizado", Toast.LENGTH_SHORT).show();
+                            username.setValue(currentUser.getDisplayName());
+                        } else {
+                            // Se produjo un error al actualizar el nombre de usuario
+                            Toast.makeText(context, "Error al actualizar el nombre de usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public MutableLiveData<String> getUsername() {
+        return username;
+    }
+
+    public MutableLiveData<String> getEmail() {
+        return email;
+    }
 }
