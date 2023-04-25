8e6f965256c1027b920d39a53858c3e40b350666
danielramos4 <danielramosperez4@gmail.com>
2023-04-25 11:03:30 +0200

    Importados los logos y creados los activities de home y lesson

---
+++ app/src/main/java/edu/pis/codebyte/view/lesson/LessonActivity.java
@@ -0,0 +1,16 @@
+package edu.pis.codebyte.view.lesson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.pis.codebyte.R;
+
+public class LessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+    }
+}
