99aedde554234f1729a65cf736b64445c480879d
danielramos4 <danielramosperez4@gmail.com>
2023-04-25 09:55:27 +0200

    //Cambios cosas de diseño

---
+++ app/src/main/java/edu/pis/codebyte/view/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package edu.pis.codebyte.view.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.pis.codebyte.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
