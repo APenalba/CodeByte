2a3a03225a73d763cfa912d684ba66af2765ae82
Alejandro Penalba <apenalbatarrida@gmail.com>
2023-04-26 13:25:30 +0200

    - El apartado de enviar un comentario o problema se parece mas al mockup

--- app/src/main/java/edu/pis/codebyte/view/profile/ProfileFragment.java
+++ app/src/main/java/edu/pis/codebyte/view/profile/ProfileFragment.java
@@ -9,13 +9,6 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +20,12 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.squareup.picasso.Picasso;
 
@@ -205,6 +204,7 @@ public class ProfileFragment extends Fragment {
                     return;
                 }
                 profileVM.enviarComentario(problemaText,mContext);
+                problema.setText("");
             }
         });
     }
