850235574277b0f3cf098b666e627fa5edbcb4d3
danielramos4 <danielramosperez4@gmail.com>
2023-05-18 15:16:28 +0200

    He hecho una splash screen para mejorar el diseño

---
+++ app/src/main/java/edu/pis/codebyte/view/main/SplashScreen.java
@@ -0,0 +1,46 @@
+
+package edu.pis.codebyte.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.pis.codebyte.R;
+import edu.pis.codebyte.view.login.LoginActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Agregamos animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView deTextView = findViewById(R.id.codeByteTextView);
+        ImageView logoImageView = findViewById(R.id.imageViewLogo);
+
+        deTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
