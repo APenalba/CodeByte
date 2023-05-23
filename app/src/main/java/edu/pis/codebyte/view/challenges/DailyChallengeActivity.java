0e1e22ba1852c3eab80810b09610455982642c2c
danielramos4 <danielramosperez4@gmail.com>
2023-05-23 10:28:21 +0200

    //Cambio boton start por continue //Fragment de daily challenge ya no está vacio, ahora hay un coming soon y hay un boton que te devuele al home

--- app/src/main/java/edu/pis/codebyte/view/challenges/DailyChallengeActivity.java
+++ app/src/main/java/edu/pis/codebyte/view/challenges/DailyChallengeActivity.java
@@ -1,10 +1,15 @@
 package edu.pis.codebyte.view.challenges;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import edu.pis.codebyte.R;
+import edu.pis.codebyte.view.main.home.HomeFragment;
 
 public class DailyChallengeActivity extends AppCompatActivity {
 
@@ -12,5 +17,28 @@ public class DailyChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_challenge);
+
+        Button returnBtn  = findViewById(R.id.button_gohome);
+
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HomeFragment homeFragment = new HomeFragment();
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                // Especificar animaciones personalizadas para la entrada y salida del Fragment
+                fragmentTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
+                        android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                fragmentTransaction.replace(R.id.fragment_container, homeFragment);
+                fragmentTransaction.addToBackStack(null);
+
+                // Realizar la transacción
+                fragmentTransaction.commit();
+
+            }
+        });
     }
+
 }
